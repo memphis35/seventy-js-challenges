a24669b5cb02ffd50e9f4897447d9be9c959e2d9
Aleksandr Smirnov <aa.smirnow@outlook.com>
2025-01-25 12:28:40 -0600

    Add another intersection implementation

--- 02-basic-challenges/02-arrays-intersection.js
+++ 02-basic-challenges/02-arrays-intersection.js
@@ -4,4 +4,15 @@ function intersection(arr1, arr2) {
     return iterated.filter(el => checked.includes(el)).sort();
 };
 
+function intersection2(arr1, arr2) {
+    const unique = new Set(arr1);
+    const result = [];
+    for (let el in arr2) {
+        if (unique.has(el)) {
+            result.push(el);
+        }
+    }
+    return result.sort();
+};
+
 module.exports = intersection;
